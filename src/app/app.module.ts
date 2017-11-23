a042aa29cd56328f43f5154c25d3c974b7f414cf
Siarhei Drozd <Siarhei.Drozd@aerlingus.com>
2017-11-23 15:22:53 +0300

    Module_1: Create basic project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main-page/main/main.component';
+import { FooterComponent } from './pages/main-page/footer/footer.component';
+import { HeaderComponent } from './pages/main-page/header/header.component';
+import { ButtonComponent } from './common/button/button.component';
+import { LogoComponent } from './common/logo/logo.component';
+import { CourseBlockComponent } from './common/course-block/course-block.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    HeaderComponent,
+    ButtonComponent,
+    LogoComponent,
+    CourseBlockComponent,
+    LoginPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/common/course-block/course-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-block',
+  templateUrl: './course-block.component.html',
+  styleUrls: ['./course-block.component.css']
+})
+export class CourseBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
