bbbad56596e14d5965899b401d0e74c50fcdb455
Siarhei Drozd <siarhei_drozd@epam.com>
2017-12-10 20:45:19 +0300

    Module_3: Remove debugger

--- src/app/pages/courses-page/courses-page.service.ts
+++ src/app/pages/courses-page/courses-page.service.ts
@@ -8,7 +8,7 @@ export class CoursesPageService {
   private dummyText: string;
   private defaultControls: Object[];
 
-  constructor() { debugger;
+  constructor() {
     this.dummyText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
