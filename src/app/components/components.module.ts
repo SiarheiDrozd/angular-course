fe614405df91bbefcfd26f58f7f139d140036fa3
Siarhei Drozd <siarhei_drozd@epam.com>
2017-12-28 00:06:54 +0300

    Module_5: Update components module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,14 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { ButtonComponent } from './button/button.component';
-import { LogoComponent } from './logo/logo.component';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { ModalComponent } from './modal/modal.component';
-import { ToolboxComponent } from './toolbox/toolbox.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import * as CommonComponents from './';
 
 @NgModule({
   imports: [
@@ -17,24 +10,10 @@ import { LoginFormComponent } from './login-form/login-form.component';
     FormsModule
   ],
   declarations: [
-    FooterComponent,
-    HeaderComponent,
-    ButtonComponent,
-    LogoComponent,
-    BreadcrumbsComponent,
-    ModalComponent,
-    ToolboxComponent,
-    LoginFormComponent
+    ...Object.values(CommonComponents)
   ],
   exports: [
-    FooterComponent,
-    HeaderComponent,
-    ButtonComponent,
-    LogoComponent,
-    BreadcrumbsComponent,
-    ModalComponent,
-    ToolboxComponent,
-    LoginFormComponent
+    ...Object.values(CommonComponents)
   ]
 })
 export class ComponentsModule { }
